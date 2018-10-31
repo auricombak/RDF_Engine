226f150d316959b9b03716ead35ab751ba9f1359
auricombak <lois.fwee@hotmail.fr>
2018-10-31 01:38:33 +0100

    Refactoring extract type in new file

--- RDFprojet/src/dictionary/Dictionary.java
+++ RDFprojet/src/dictionary/Dictionary.java
@@ -1,31 +1,97 @@
 package dictionary;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Dictionary{
+	
+	//Dictionary with objects, predicates and subject sorted | TreeMap<Key,Value> -> <String,Integer>
 	TreeMap < String, Integer > dico;
 	
-	public Dictionary() {
-		this.dico = new TreeMap<>();
-	}
+	//Base with objects, predicates and subject, for retrieve value in fast access | HashMap<Key,Value> -> <Integer,Value>
+	HashMap<Integer, String> base;
 	
-	public Dictionary(TreeMap<String, Integer> dico) {
-		this.dico = new TreeMap<>();
-		this.dico = dico;
+	//Constructor
+	public Dictionary(RDFListener list) {
+		
+		dico =  new TreeMap<>();
+		base = new HashMap<>();
+		
+		TreeSet<String> listEachSPO = new TreeSet<>();
+		
+		
+		// Populate TreeSet who sort and avoid doublons
+		int i=0;
+		while(i<list.nbTriple){
+			
+			listEachSPO.add(list.subjects.get(i));
+			listEachSPO.add(list.predicates.get(i));
+			listEachSPO.add(list.objects.get(i));			
+			
+			i++;
+		}
+		
+		// Populate dico & base
+		int cmp=0;
+		for(String value : listEachSPO){
+			dico.put(value, cmp);
+			base.put(cmp, value);
+			cmp++;
+			
+			//System.out.println("<"+value+" , "+cmp+">");
+		}
 	}
 
+	//Getter Dico
 	public TreeMap<String, Integer> getDico() {
 		return dico;
 	}
 
+	//Setter Dico
 	public void setDico(TreeMap<String, Integer> dico) {
 		this.dico = dico;
 	}
 
+	//Getter base
+	public HashMap<Integer, String> getBase() {
+		return base;
+	}
 
+	//Setter base
+	public void setBase(HashMap<Integer, String> base) {
+		this.base = base;
+	}
+
+	//Write the content of the dico -> dico.txt
+	public void writeDico() throws IOException {
+		String aggFileName = String.valueOf("dico.txt");
+		FileWriter fstream = new FileWriter(aggFileName);
+		BufferedWriter out = new BufferedWriter(fstream);
+		
+		for (Map.Entry<String, Integer> entree : dico.entrySet()) {
+		    out.write("Key-["+entree.getKey() + "]" + " | Value-[" + entree.getValue() + "]\n");
+		    out.flush();
+
+		}
+	}
 	
-	
-	
+	//Write the content of the base -> base.txt
+	public void writeBase() throws IOException {
+		String aggFileName = String.valueOf("base.txt");
+		FileWriter fstream = new FileWriter(aggFileName);
+		BufferedWriter out = new BufferedWriter(fstream);
+		
+		for (Entry<Integer, String> entree : base.entrySet()) {
+		    out.write("Key-["+entree.getKey() + "]" + " | Value-[" + entree.getValue() + "]\n");
+		    out.flush();
+
+		}
+	}
 	
 }
--- RDFprojet/src/dictionary/RDFRawParser.java
+++ RDFprojet/src/dictionary/RDFRawParser.java
@@ -26,33 +26,6 @@ import org.openrdf.rio.helpers.RDFHandlerBase;
 
 public final class RDFRawParser {
 	
-	private static 	TreeMap <String, Integer > dico = new TreeMap<>();
-	private static HashMap<Integer, String> base = new HashMap<>();
-	
-	//On recherchera toujours le sujet donc il est plus performant de le garder en fin pour une meilleure performance
-	private static HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> pos =  new HashMap<>();
-	private static HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> ops =  new HashMap<>();
-
-		
-	private static class RDFListener extends RDFHandlerBase {
-
-		public ArrayList<String > subjects = new ArrayList<>();
-		public ArrayList<String > predicates = new ArrayList<>();
-		public ArrayList<String > objects = new ArrayList<>();
-		static Integer nbTriple = 0;
-		
-		@Override
-		public void handleStatement(Statement st) {
-
-			
-			subjects.add(st.getSubject().toString());
-			predicates.add(st.getPredicate().toString());
-			objects.add(st.getObject().toString().replaceAll("\"", ""));						
-			nbTriple++;
-		}
-
-	};
-
 	public static void main(String args[]) throws IOException {
 
 		
@@ -69,109 +42,33 @@ public final class RDFRawParser {
 		
 		try {reader.close();} catch (IOException e) {}
 		
-		init(list);
-
+		Dictionary dico = new Dictionary(list);
+		Index index = new Index(dico, list);
 		
-		/*
-		for(int x=0; x<200; x++) {
-			System.out.println("------");
-			System.out.println("Predicate : ["+list.predicates.get(x).toString() + "]");
-			System.out.println("Object : ["+list.objects.get(x).toString() + "]");
-			System.out.println("Subject : ["+list.subjects.get(x).toString()+ "]");
-
-		} */
+		// dico.writeDico();
+		// dico.writeBase();
 		
-		//displayDico();
-		
-		System.out.println("Result : " + query("http://db.uwaterloo.ca/~galuc/wsdbm/userId", "9279708" ).toString());
+		System.out.println("Result : " + query(index, dico , "http://db.uwaterloo.ca/~galuc/wsdbm/userId", "9279708" ).toString());
 
 	}
 	
-	public static void init( RDFListener list){
-		
-		TreeSet<String> listEachSPO = new TreeSet<>();
-		
-		
-		// Populate TreeSet who sort and avoid doublons
-		int i=0;
-		while(i<list.nbTriple){
-			
-			listEachSPO.add(list.subjects.get(i));
-			listEachSPO.add(list.predicates.get(i));
-			listEachSPO.add(list.objects.get(i));			
-			
-			i++;
-		}
-		
-		// Populate dico & base
-		int cmp=0;
-		for(String value : listEachSPO){
-			dico.put(value, cmp);
-			base.put(cmp, value);
-			cmp++;
-			
-			//System.out.println("<"+value+" , "+cmp+">");
-		}
-		
-		
-		// Create Index ops and pos
-		int j=0;
-		while(j<list.nbTriple){
-			
-			int o = dico.get(list.objects.get(j));
-			int p = dico.get(list.predicates.get(j));
-			int s = dico.get(list.subjects.get(j));
-			
-			ops.putIfAbsent(o, new HashMap<Integer, ArrayList<Integer> >() ) ;
-			HashMap<Integer, ArrayList<Integer>> hashOps = ops.get(o); 
-			hashOps.putIfAbsent(p, new ArrayList<Integer>());
-			hashOps.get(p).add(s);
-			
-			pos.putIfAbsent(p, new HashMap<Integer, ArrayList<Integer> >() ) ;
-			HashMap<Integer, ArrayList<Integer>> hashPos = pos.get(p); 
-			hashPos.putIfAbsent(o, new ArrayList<Integer>());
-			hashPos.get(o).add(s);
-			
-			j++;
-		}
-		
-		//System.out.println(pos.size());
-		//System.out.println(ops.size());
-		//System.out.println(pos.size());
-	
-		//System.out.println(ops.toString());
 
-	}
 
-	public static ArrayList<String> query( String predicate, String object) {
+	public static ArrayList<String> query(Index index, Dictionary dico, String predicate, String object) {
 		
-		int Ipredicate = dico.get(predicate);
-		int Iobject = dico.get(object);
+		int Ipredicate = dico.getDico().get(predicate);
+		int Iobject = dico.getDico().get(object);
 		
-		ArrayList<Integer> Isubject = pos.get(Ipredicate).get(Iobject);
+		ArrayList<Integer> Isubject = index.getPos().get(Ipredicate).get(Iobject);
 		ArrayList<String> subject = new ArrayList<>();
 		
 		
 		for(Integer s: Isubject) {
-			subject.add(base.get(s));
+			subject.add(dico.getBase().get(s));
 		}
 		
 		
 		return subject;
 		
-	}
-	
-	public static void displayDico() throws IOException {
-		String aggFileName = "agg-"+String.valueOf("06.txt");
-		FileWriter fstream = new FileWriter(aggFileName);
-		BufferedWriter out = new BufferedWriter(fstream);
-		
-		for (Map.Entry<String, Integer> entree : dico.entrySet()) {
-			System.out.println("Clé : "+entree.getKey()+" Valeur : "+entree.getValue());
-		    out.write("Key "+entree.getKey() + "\t" + "Value" + entree.getValue() + "\n");
-		    System.out.println("Done");
-		    out.flush();
-
-		}
 	}
 }
